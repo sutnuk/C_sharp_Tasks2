f14edc3ddc918bbba8f689a7dcd2ac6ed62eb451
Nazar Sytnyk <sutnuk23nazar@gmail.com>
2019-03-23 19:28:39 +0200

    Complite Task1

---
+++ Task/Task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1 task1 = new Task1();
+            DateTime first = new DateTime(2009, 1, 1);
+            DateTime second = new DateTime(2010, 1, 1);
+            int j = 0;
+            foreach (DateTime i in task1.ChooseDate(first, second))
+            {
+                Console.WriteLine(i);
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task/Task/Task1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task
+{
+    class Task1
+    {
+        private DateTime minDate;
+        private DateTime maxDate;
+        private  Random rand;
+
+        public DateTime MinDate { get => minDate; set => minDate = value; }
+        public DateTime MaxDate { get => maxDate; set => maxDate = value; }
+        public Random Rand { get => rand; set => rand = value; }
+
+        public Task1() { }
+
+        public Task1(DateTime minDate, DateTime maxDate)
+        {
+            this.minDate = minDate;
+            this.maxDate = maxDate;
+        }
+
+        public IEnumerable<DateTime> ChooseDate(DateTime minDate, DateTime maxDate)
+        {
+            Rand = new Random();
+            int range = (maxDate - minDate).Days;
+            yield return minDate.AddDays(Rand.Next(range));
+        }
+    }
+    
+}
